f30adcc4e6f557fca84bd4103d81b1ed59d3a610
bode135 <2248270222@qq.com>
2022-07-04 15:45:13 +0800

    获取cpu信息函数

--- bdtime/utils.py
+++ bdtime/utils.py
@@ -1,19 +1,16 @@
 import json
 
 
-def show_json(data: (dict, str), sort_keys=False):
-    if isinstance(data, dict):
-        for k, v in data.items():
-            print(k, ' --- ', v)
-    else:
-        try:
-            print(json.dumps(data, sort_keys=sort_keys, indent=4, separators=(', ', ': '), ensure_ascii=False))
-        except Exception as e:
-            try:
-                for k, v in data:
-                    print(k, ' --- ', v)
-            except Exception as e:
-                raise e
+def show_json(data: dict, sort_keys=False):
+    try:
+        print(json.dumps(data, sort_keys=sort_keys, indent=4, separators=(', ', ': '), ensure_ascii=False))
+    except:
+        if isinstance(data, dict):
+            for k, v in data.items():
+                print(k, ' --- ', v)
+        else:
+            for k, v in data:
+                print(k, ' --- ', v)
 
 
 def show_ls(data: list, ks=None):
--- test.py
+++ test.py
@@ -1,15 +1,18 @@
 from bdtime import version, tt
+from bdtime.get_cpu_info import main as get_cpu_info
 
 
 if __name__ == '__main__':
     print("--- bdtime version:", version())
 
-    # 默认保留2位小数
-    ret = tt.get_current_beijing_time_str()
-    print(ret)
+    get_cpu_info()
 
-    # 这个适合做临时文件的文件名后缀
-    ret = tt.get_current_beijing_time_str(tt.common_date_time_formats.ms_int)
-    print(ret)
+    # # 默认保留2位小数
+    # ret = tt.get_current_beijing_time_str()
+    # print(ret)
+    #
+    # # 这个适合做临时文件的文件名后缀
+    # ret = tt.get_current_beijing_time_str(tt.common_date_time_formats.ms_int)
+    # print(ret)
 
 
